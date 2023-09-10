94027bb5c99d6ca415a49f564ab2047212b2005a
adix2326 <aditya1dhamale1@gmail.com>
2023-09-10 09:59:51 +0530

    second commit

--- hello1.java
+++ hello1.java
@@ -1,6 +1,6 @@
-class hello{
+public class hello1 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello1");
         System.out.println("world");
     }
 }
